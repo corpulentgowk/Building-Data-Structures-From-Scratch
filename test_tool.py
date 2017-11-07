626bdfc3c085bf1f977baabe2ac03e5f07364620
Steven Layne <stevenzmanx@u.northwestern.edu>
2017-11-07 10:34:24 -0800

    Added Feedback on what was expected versus what was gotten back. Also added the ability to test for truthness

--- test_tool.py
+++ test_tool.py
@@ -8,11 +8,26 @@ class test(object):
 		if str(self.dataStruct) == expectedResult:
 			self.passed = self.passed and True
 		else:
+			testCase += [str(self.dataStruct), "Expected: '" + str(expectedResult) + "' Got: '" + str(self.dataStruct) + "'. "]
+			self.failedCases.append(testCase)
+			self.passed = False
+	def validateTruth(self, expectedResult, truthness, testCase):
+		if truthness == expectedResult:
+			self.passed = self.passed and True
+		else:
+			testCase += [str(self.dataStruct), "Expected: '" + str(expectedResult) + "' Got: '" + str(truthness) + "'. "]
 			self.failedCases.append(testCase)
 			self.passed = False
 
-	def executeCommands(self, func, inputs, expectedResult):
+	def executeCommands(self, func, inputs, expectedResult, truthy=False):
 		method = getattr(self.dataStruct, func)
-		for inpt in inputs:
-			method(inpt)
-		self.validateResult(expectedResult, [func, inputs])
+		if not truthy:
+			if inputs:
+				for inpt in inputs:
+					method(inpt)
+			else:
+				method()
+			self.validateResult(expectedResult, [func, inputs])
+		else:
+			truthness = method()
+			self.validateTruth(expectedResult, truthness, [func])
