a69493af9dac06eff5796032b7410af7d4238cdb
Steven Layne <stevenzmanx@u.northwestern.edu>
2017-11-10 08:56:57 -0800

    Added load balancing for insertions and removals

--- HashTable-Using-LinkLists/HashTable.py
+++ HashTable-Using-LinkLists/HashTable.py
@@ -5,9 +5,24 @@ class HashBucket(object):
 	def __init__(self, size=131): #131 is a prime number
 		self.size = size
 		self.bucket = [None] * size
+		self.numElements = 0
 
 	def hashFunction(self, key):
 		return hash(key) % self.size
+
+	def rehash(self, newSize):
+		self.size = newSize
+		oldBucket = self.bucket
+
+		self.bucket = [None] * newSize
+		self.numElements = 0
+
+		for chain in oldBucket:
+			if chain:
+				curr = chain.root
+				while curr:
+					self.insert(curr.key, curr.val)
+					curr = curr.next
 
 	def insert(self, key, value):
 		ind = self.hashFunction(key)
@@ -27,14 +42,16 @@ class HashBucket(object):
 			print "KeyError: '" + str(key) + "'"
 			return 
 
-		if not self.bucket[ind].root: #Checks if Removal was off the root node in Linked List
-			self.bucket[ind] = None #If so reset the bucket to None
 
 	def createChain(self, ind, key, value):
 		chain = LinkedList()
-
 		chain.insertVal(key,value)
 		self.bucket[ind] = chain
+
+		self.numElements += 1
+
+		if (float(self.numElements)/float(self.size)) > .75:
+			self.rehash(self.size << 1) #Double Size of Hash Table and Rehash the elements
 
 	def addToChain(self, ind, key, value):
 		chain = self.bucket[ind]     #Have to search through the nodes manually
@@ -48,21 +65,43 @@ class HashBucket(object):
 
 			if not curr.next:
 				curr.next = ListNode(key,value)
+				self.numElements += 1
+
+				if (float(self.numElements)/float(self.size)) > .75:
+					self.rehash(self.size << 1) #Double Size of Hash Table and Rehash the elements
+
 				break
 			curr = curr.next
 
 	def removeFromChain(self, ind, key):
-		chain = self.bucket[ind].removeOne(key)
+		chain = self.bucket[ind]
+
+		chain.removeOne(key)
+
+		if not self.bucket[ind].root: #Checks if Removal was off the root node in Linked List
+			self.bucket[ind] = None #If so reset the bucket to None
+
+		self.numElements -= 1
+		if (float(self.numElements)/float(self.size)) < .25:
+			self.rehash(self.size >> 1) #Halve the Size of Hash Table and Rehash the elements
 
 
 '''
-Hash = HashBucket()
+Hash = HashBucket(4) #Init HashMap of size 4
 
-Hash.insert(1,"wrecked")
-Hash.insert(132,"wrecked")
-Hash.insert(132,"Change of Heart")
-Hash.insert(133, "hey")
-Hash.insert(2, "hi")
+Hash.insert(1,"one")
+Hash.insert(2,"two")
+Hash.insert(3, "three")
+Hash.insert(4, "four")
+
+Hash.remove(2)
+Hash.remove(1)
+Hash.remove(3)
+
+Hash.insert(6, "six")
+
+
+print Hash.bucket
 
 for l in Hash.bucket:
 	if l:
